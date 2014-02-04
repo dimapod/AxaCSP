e4b817cf19d69709ef2673929b7fd79d4b7f0661
dimapod <dimapod@gmail.com>
2014-02-04 10:37:58 +0100

    Clean up

--- app/scripts/menu/appCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('axa.csp').controller('appCtrl', function($scope, notifications) {
-    $scope.$on('$routeChangeError', function(event, current, previous, rejection){
-        notifications.pushForCurrentRoute('Route change error', 'error');
-    });
-});
